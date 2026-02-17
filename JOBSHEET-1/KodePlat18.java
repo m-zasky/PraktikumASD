d435ac529c15d92dc2533071e9ef6535dcab4d7f
m-zasky <mmuhammadzasky@gmail.com>
2026-02-17 20:22:17 +0700

    KodePlat18

---
+++ JOBSHEET-1/KodePlat18.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class KodePlat18 {
+    public static void main(String[] args) {
+
+        // deklarasi array 1 & 2 dimensi
+        char[] KODE = {'A', 'S', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T'};
+        char[][] KOTA = {
+            {'B','A','N','T','E','N'}, {'J','O','M','B','A','N','G'},
+            {'B','A','N','D','U','N','G'}, {'C','I','R','E','B','O','N'},
+            {'B','O','G','O','R'}, {'P','E','K','A','L','O','N','G','A','N'},
+            {'S','E','M','A','R','A','N','G'}, {'S','U','R','A','B','A','Y','A'},
+            {'M','A','L','A','N','G'}, {'T','E','G','A','L'}
+        }; 
+
+        // user menginputkan kode plat
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Kode Plat: ");
+        char input = sc.next().toUpperCase().charAt(0);
+
+
+        // Kota berdasarkan kode plat
+        System.out.print("Kota Berdasarkan Kode Plat Adalah : ");
+        for (int i = 0; i < KODE.length; i++) {
+
+            if (KODE[i] == input) {
+                for (char c : KOTA[i]) System.out.print(c);
+                return;
+            }
+        }
+        System.out.println("Kode tidak ditemukan");
+
+        sc.close();
+    }
+}
