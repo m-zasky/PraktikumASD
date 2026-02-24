4c7104bd49878c121a2c5ca98d2a638da17e99e7
MUHAMMAD ZASKY ARYO MAHADANI <mmuhammadzasky@gmail.com>
2026-02-24 22:38:22 +0700

    ModifCetakInfo

--- JOBSHEET-3/MahasiswaDemo18.java
+++ JOBSHEET-3/MahasiswaDemo18.java
@@ -61,13 +61,20 @@ public class MahasiswaDemo18 {
             System.out.println("--------------------------------");
         }
 
+        // for (int i = 0; i < 3; i++) {
+        // System.out.println("Data Mahasiswa ke-" + (i + 1));
+        // System.out.println("NIM : " + arrayOfMahasiswa[i].nim);
+        // System.out.println("Nama : " + arrayOfMahasiswa[i].nama);
+        // System.out.println("Kelas : " + arrayOfMahasiswa[i].kelas);
+        // System.out.println("IPK : " + arrayOfMahasiswa[i].ipk);
+        // System.out.println("---------------------------------");
+        // }
+
+        // modif cetakInfo
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
-            System.out.println("---------------------------------");
+            arrayOfMahasiswa[i].cetakInfo(); // cukup panggil method ini
+            System.out.println("----------------------------------");
         }
 
         sc.close();
