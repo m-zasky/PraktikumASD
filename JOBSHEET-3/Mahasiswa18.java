638014d4b494aef494f65c1841276fb2095053a0
MUHAMMAD ZASKY ARYO MAHADANI <mmuhammadzasky@gmail.com>
2026-02-24 22:37:33 +0700

    TambahMethodCetakInfo

--- JOBSHEET-3/Mahasiswa18.java
+++ JOBSHEET-3/Mahasiswa18.java
@@ -3,4 +3,12 @@ public class Mahasiswa18 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    // menambahkan method cetak info
+    public void cetakInfo() {
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+    }
 }
